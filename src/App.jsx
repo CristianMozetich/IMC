958f129ba3ac1bfd34310bc8eb1fd25713097c85
Cristian Mozetich <cristianmozetich@gmail.com>
2023-07-01 15:09:05 +0100

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import IMCCalculator from './components/ImcCalculator/ImcCalculator'
+import Intro from './components/Intro/Intro'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h1>Índice de masa corporal</h1>
+      <Intro/>
       <IMCCalculator/>
     </>
   )
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,11 @@
+
+
+const Intro = () => {
+  return (
+    <div>
+      <h3>El sobrepeso puede causar la elevación de la concentración de colesterol total y de la presión arterial, y aumentar el riesgo de sufrir la enfermedad arterial coronaria. La obesidad aumenta las probabilidades de que se presenten otros factores de riesgo cardiovascular, en especial, presión arterial alta, colesterol elevado y diabetes.</h3>
+    </div>
+  )
+}
+
+export default Intro
