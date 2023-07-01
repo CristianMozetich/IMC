57f83f6dad9076ed2ca336cf24bd81f0f23598c3
Cristian Mozetich <cristianmozetich@gmail.com>
2023-07-01 21:54:57 +0100

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
       <div className="footer">
         <h4>Desarrollado por <a target='_blank' href="https://cristianmozetich.netlify.app/">Cristian Mozetich</a></h4>
-        <p>Movimiento y Salud | Todos los derechos reservados | 2023</p>
+        <p>Movimiento y Salud | 2023</p>
       </div>
     )
   }
