deab32d3314f49563fb0e0d4c2099f6ce3ccdef8
Cristian Mozetich <cristianmozetich@gmail.com>
2023-07-02 15:01:07 +0100

    ultimas mod

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,7 +3,7 @@ import './Intro.css'
 const Intro = () => {
   return (
     <div className="intro">
-      <p>El Índice de Masa Corporal (IMC) es una herramienta para evaluar la relación entre el peso y la estatura de una persona. Se utiliza para determinar si alguien tiene un peso saludable o si está en riesgo de problemas de salud ocasionados por la hipertensión y los altos niveles de colesterol en sangre relacionados con el peso. Sin embargo, el IMC no considera otros factores importantes, como la distribución de la grasa corporal. Por lo tanto, se recomienda complementarlo con otras evaluaciones médicas para obtener una imagen completa de la salud.</p>
+      <p>El Índice de Masa Corporal (IMC) es una herramienta para evaluar la relación entre el peso y la estatura de una persona. Se utiliza para determinar si alguien tiene un peso saludable o si está en riesgo de problemas de salud ocasionados por la hipertensión y los altos niveles de colesterol en sangre. Sin embargo, el IMC no considera otros factores importantes, como la distribución de la grasa corporal. Por lo tanto, se recomienda complementarlo con otras evaluaciones médicas para obtener una imagen completa de la salud.</p>
     </div>
   )
 }
